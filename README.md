fc78131fdb65b92394c86efe3b3a81da483a3ac3
guanho <guanho@guanhoui-MacBook-Pro.local>
2016-07-03 01:04:48 +0900

    script common first commit

---
+++ README.md
@@ -0,0 +1,87 @@
+# Crop
+### JavaScript
+
+Quick ImageCrop JavaScript
+
+## Example
+
+**Create .html**
+
+**add jQuery.js**
+
+**add common.js**
+
+~~~~
+
+
+<div id="util"></div><br>
+<div id="replace"></div><br>
+<div id="commaPrice"></div><br>
+<div id="list"></div>
+
+
+<script type="text/javascript">
+$(document).ready(function(){
+    /** util Test **/
+    var util = $.util();
+    var platform = util.platform;
+    var browser = util.browser;
+    var newOpen = util.newOpen;
+    $('#util').append('platform = '+platform);
+    $('#util').append('<br>browser = '+browser);
+    $('#util').append('<br>newOpen = '+newOpen);
+
+
+
+
+    /** createForm, createInput Test **/
+    var form = $.createForm('put');
+    $.createInput(form,'foo','bar');
+    $.createInput(form,'boo','bar2');
+
+
+    /** ajax Test **/
+    // jQuery.ajaxFnImage(url,form,method,function(data){
+    // 	console.log(data);
+    // },false);
+    // jQuery.ajaxFn(url,form,method,function(data){
+    // 	console.log(data);
+    // },false);
+
+
+    /** replaceAll Test **/
+    var text = 'hello javascript world';
+    var replaceText = text.replace('javascript', 'jQuery');
+    $('#replace').html('replaceText = '+replaceText);
+
+
+    /** setPriceComma Test **/
+    var price = '111222333444';
+    var commaPrice = price.setPriceComma();
+    $('#commaPrice').html('commaPrice = '+commaPrice);
+
+
+    /** list remove Test **/
+    var array = ['foo', 'boo', 'coo?', 'doo?'];
+    array.remove(2);
+    $('#list').html(JSON.stringify(array));
+});
+</script>
+~~~~
+
+##Image
+
+###chromeTest Image
+![Alt text](/exampleImages/chromeTest.png)
+
+###safariTest Image
+![Alt text](/exampleImages/safariTest.png)
+
+
+## Author
+
+pikachu987, pikachu987@naver.com
+
+## License
+
+Crop is available under the MIT license. See the LICENSE file for more info.
---
+++ common.js
@@ -0,0 +1,113 @@
+$(function(){
+	$.extend({
+		util : function(){
+			var returnType = {};
+			var navigatorAgent = navigator.userAgent.toLowerCase();
+			if(navigatorAgent.indexOf("iphone") > 0 || navigatorAgent.indexOf("ipod") > 0){returnType.platform = 'ios';}
+			else if(navigatorAgent.indexOf("android ") > 0){returnType.platform = 'and';}
+			else{returnType.platform = 'des';}
+			if(returnType.platform == 'des'){
+				if(navigatorAgent.indexOf('chrome') != -1){ returnType.browser = 'chrome';}
+				else if(navigatorAgent.indexOf('net') != -1){ returnType.browser = 'ie';}
+				else{ returnType.browser = 'safari';}
+			}else{
+				if(navigatorAgent.indexOf('kakao') != -1){ returnType.browser = 'kakao';}
+				else if(navigatorAgent.indexOf('fb') != -1){ returnType.browser = 'face';}
+				else if(navigatorAgent.indexOf('naver') != -1){ returnType.browser = 'naver';}
+				else if(navigatorAgent.indexOf('daum') != -1){ returnType.browser = 'daum';}
+				else{ returnType.browser = 'safari';}
+			}
+			try{
+				if(window.opener === null || window.opener == 'null') returnType.newOpen = false;
+			}catch(e){
+				returnType.newOpen = true;
+			}
+			return returnType;
+		},
+		createForm : function(method,name){
+			var form = document.createElement("form");
+			if(name == undefined){
+				form.setAttribute("name", 'utilTempForm');
+			}else{
+				form.setAttribute("name", name);
+			}
+			form.setAttribute("method", method);
+			document.body.appendChild(form);
+			$(form).hide();
+			if(method.toLowerCase() == 'put' || method.toLowerCase() == 'delete'){
+				jQuery.createInput(form, "_method", method);
+			}
+			return form;
+		},
+		createInput : function(form, name, value){
+			var input = document.createElement("input");
+			input.setAttribute("type", "hidden");
+			input.setAttribute("name", name);
+			input.setAttribute("value", value);
+			form.appendChild(input);
+		},
+		ajaxFnImage : function(url,form,method,requestFunction,async){
+			var formData = new FormData(form);
+			$.ajax({
+				type:method,
+				url:url,
+				data:formData,
+				processData:false,
+				contentType:false,
+				dataType:"json",
+				async:async,
+				success:function(data){
+					requestFunction(data);
+				},error:function(jqXHR,textStatus,errorThrown){
+					jQuery.ajaxError(jqXHR,textStatus,errorThrown);
+				}
+			});
+		},
+		ajaxFn : function(url,form,method,requestFunction,async,tempData){
+			$.ajax({
+				type:method,
+				url:url,
+				async:async,
+				data:$(form).serialize(),
+				success:function(data){
+					requestFunction(data,tempData);
+				},error:function(jqXHR,textStatus,errorThrown){
+					jQuery.ajaxError(jqXHR,textStatus,errorThrown);
+				}
+			});
+		},
+		ajaxError : function(jqXHR,textStatus,errorThrown){
+			var errorMsg = '';
+			errorMsg += '-jqXHR.status:'+jqXHR.status+'\n';
+			errorMsg += '-jqXHR.statusText:'+jqXHR.statusText+'\n';
+			errorMsg += '-jqXHR.responseText:'+jqXHR.responseText+'\n';
+			errorMsg += '-jqXHR.readyState:'+jqXHR.readyState+'\n';
+			errorMsg += '-textStatus:'+textStatus+'\n';
+			errorMsg += '-errorThrown:'+errorThrown+'\n';
+			console.log(errorMsg);
+		}
+	});
+	Array.prototype.remove = function(idx){
+		var temp = [];
+		var i = this.length;
+		while(i > idx){
+			var kk = this.pop();
+			temp.push(kk);
+			i--;
+		}
+		for(var j=temp.length - 2; j>=0; j--){
+			this.push(temp[j]);
+		}
+	};
+	String.prototype.replaceAll = function(find,replace){
+		if(this === null || this === '') return '';
+		return this.replace( new RegExp( find, 'g' ), replace );
+	};
+	String.prototype.setPriceComma = function() {
+		var n = this;
+		n += '';
+		var reg = /(^[+-]?\d+)(\d{3})/;
+		while (reg.test(n)){n = n.replace(reg, '$1' + ',' + '$2');}
+		return n;
+	};
+});
